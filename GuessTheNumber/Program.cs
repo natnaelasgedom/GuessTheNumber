948d3b5a17bade5697098305d8f7d42ee9bdfc3f
kkilli <kristian.killi@hotmail.com>
2020-06-12 13:13:20 +0200

    Run Method with errors

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,7 +6,28 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var random = new Randoms();
+            random.Run();
+        }
+    }
+    class Randoms
+    {
+        public void Run()
+        {
+            int options = 500;
+            while (true)
+            {
+                Console.WriteLine("Is the number {0}", options + "h or l?");
+                string answer = Console.ReadLine();
+                if (answer == "h")
+                {
+                    options += options / 2;
+                }
+                else
+                {
+                    options -= options / 2;
+                }
+            }
         }
     }
 }
